8208a57b0e3b6c4a5a209884550135808ac30592
laylahammoury <laylahammoury@gmail.com>
2019-09-11 22:18:48 +0300

    adding The attributes of pen and override the __clone function

--- Pen.php
+++ Pen.php
@@ -4,4 +4,17 @@
 class Pen extends Stationery
 {
 
+    var string $color;
+    var string $companyName;
+    var bool $transparent ;
+
+    public function __clone()
+    {
+        parent::__clone();
+        $this->color = $this->color;
+        //The companyName of the pen remains the same. Therefore we leave the reference to the existing object
+        $this->transparent = $this->transparent;
+    }
+
+
 }
