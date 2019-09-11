dfe2794838929126076a1c00529c564135e733fc
laylahammoury <laylahammoury@gmail.com>
2019-09-11 22:17:09 +0300

    adding The attributes of stationary and implementing hte __clone function

--- Stationery.php
+++ Stationery.php
@@ -3,9 +3,23 @@
 
 class Stationery implements prototypeInterface
 {
+    var string $description;
+    var string $owner;
+    var DateTime $purchaseDate;
+    //getters
+    public function getDescription() : string     { return $this->description;}
+    public function getOwner() : string     { return $this->owner;}
+    public function getPurchaseDate() : string      { return $this->purchaseDate;}
 
-    function cloning()
+    //setters
+    public function setDescription($description)    {$this->description = $description;}
+    public function setOwner($owner)    {$this->owner = $owner;}
+    public function setPurchaseDate($purchaseDate)    {$this->purchaseDate = $purchaseDate;}
+
+    function __clone()
     {
-        // TODO: Implement cloning() method.
+        $this->description = "Copy of " . $this->description;
+        //The owner of the stationary remains the same. Therefore we leave the reference to the existing object
+        $this->purchaseDate = new DateTime;
     }
 }
