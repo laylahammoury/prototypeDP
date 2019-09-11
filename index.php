ea1bda3cb9286dde5f2e7b55e3a17c3cfa1619f2
laylahammoury <laylahammoury@gmail.com>
2019-09-12 00:04:16 +0300

    creating redPen and testing the functions

--- index.php
+++ index.php
@@ -1,8 +1,27 @@
+
+<?php
+include "Stationery.php";
+include "Pen.php";
+$redPen = new Pen("pointed pen 0.4", "Layla",
+    new DateTime(),"red", "ADEL",
+    false );
+
+
+?>
+
 <!DOCTYPE html>
 <html lang = "en-us">
 
     <body>
         <h1>header 1</h1>
+        <p><?php
+            echo "The company name is :" . $redPen->getCompanyName() ?></p>
+    <p>
+       <?php echo "the color is " . $redPen->getColor();?>
+       <?php echo "the owner is " . $redPen->getOwner();?>
+    </p> <p>
+       <?php
+       echo"purchase date is :". $redPen->getPurchaseDate()->format('Y-m-d');
+       ?>
+    </p>
     </body>
-
-<?php
