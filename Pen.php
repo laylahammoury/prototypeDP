dded5405153b910ee628ba53bf289151f58f51df
laylahammoury <laylahammoury@gmail.com>
2019-09-12 03:52:53 +0300

    Remove transparent attribute

--- Pen.php
+++ Pen.php
@@ -6,14 +6,14 @@ class Pen extends Stationery
     //it's supposed that php7.4 supports the Scalar Type Declarations but it didn't work in the browser
     var /*string*/ $color;
     var /*string*/ $companyName;
-    var /*bool*/ $transparent ;
+
 
     public function __construct($description, $owner, $purchaseDate, $color, $companyName, $transparent )
     {
         parent::__construct($description, $owner, $purchaseDate);
         $this->color = $color;
         $this->companyName = $companyName;
-        $this->transparent = $transparent;
+
     }
 
 
@@ -23,18 +23,18 @@ class Pen extends Stationery
         parent::__clone();
         $this->color = $this->color;
         //The companyName of the pen remains the same. Therefore we leave the reference to the existing object
-        $this->transparent = $this->transparent;
+
     }
 
     //getters
     public function getColor(): string   {return $this->color;}
     public function getCompanyName(): string    {return $this->companyName;}
-    public function isTransparent(): bool   {return $this->transparent;}
+
 
     //setters
     public function setColor(string $color): void   {$this->color = $color;}
     public function setCompanyName(string $companyName): void   {$this->companyName = $companyName;}
-    public function setTransparent(bool $transparent): void {$this->transparent = $transparent;}
+
 
 
 }
